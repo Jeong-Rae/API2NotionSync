c618a4a1aa118976234131a58bb54e7f3309b85c
Jeong-Rae <kkwjdfo@gmail.com>
2023-12-02 00:18:43 +0900

    [FIX] md inner ref 제거 기능 promise로 작업 마무리 확인

---
+++ parser/RemoveInnerRef.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const path = require("path");
+const util = require("util");
+const writeFile = util.promisify(fs.writeFile); // 프로미스 기반 writeFile
+const readFile = util.promisify(fs.readFile); // 프로미스 기반 readFile
+const readdir = util.promisify(fs.readdir); // 프로미스 기반 readdir
+
+const tempDirectory = "../test";
+
+async function removeInnerRef() {
+    console.log("== DO REMOVE INNER REF ==");
+    try {
+        const files = await readdir(tempDirectory);
+
+        const writeOperations = files.map(async (file) => {
+            if (path.extname(file) === ".md") {
+                const filePath = path.join(tempDirectory, file);
+                const data = await readFile(filePath, "utf8");
+
+                // 내부 참조 링크 제거
+                const updatedMarkdown = data.replace(
+                    /\[([^\]]+)\]\(#.+?\)/g,
+                    "$1"
+                );
+
+                // 결과를 같은 이름의 파일로 저장
+                const newFilePath = path.join(tempDirectory, `${file}`);
+                await writeFile(newFilePath, updatedMarkdown, "utf8");
+                console.log(`참조 제거 : ${file}`);
+            }
+        });
+
+        // 모든 파일 저장 대기
+        await Promise.all(writeOperations);
+        console.log("== FIN REMOVE INNER REF ==");
+    } catch (err) {
+        console.error("오류 발생:", err);
+    }
+}
+
+removeInnerRef();
