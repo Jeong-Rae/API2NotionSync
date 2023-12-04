10ea1b1dc3b80a16fa5a0e33469118ad530b0e96
Jeong-Rae <kkwjdfo@gmail.com>
2023-12-05 02:41:40 +0900

    [Fix] 경로 수정

---
+++ Run.js
@@ -0,0 +1,29 @@
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+
+const commands = [
+    "node ./src/SaveApiDocs",
+    "node ./node_modules/widdershins/widdershins.js ./resource/api-docs.yaml -o ./test/docs.md --summary true  --omitHeader true --code true --resolve true",
+    "node ./src/ParseMarkdownByTag.js",
+    "rm ./test/docs.md",
+    "node ./src/RemoveInnerRef.js",
+    "node ./src/MdToNotionForDir.js",
+    "node ./src/PostTagOnPage.js",
+];
+
+async function runCommand() {
+    for (const command of commands) {
+        try {
+            const { stdout, stderr } = await exec(command);
+            console.log(stdout);
+            if (stderr) {
+                console.error(`Stderr: ${stderr}`);
+            }
+        } catch (error) {
+            console.error(`Error executing ${command}: ${error.message}`);
+            break;
+        }
+    }
+}
+
+runCommand();
